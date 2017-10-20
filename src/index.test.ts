b4f972289a5a6f2a780a4e09597f6348f41f1d71
NISHI Satoshi <nishi@flexsche.com>
2017-10-20 11:25:46 +0900

    オブジェクト同士の比較

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,41 @@
 import * as assert from 'power-assert';
 
-describe('First Test', () => {
-    let author = "satocw";
-    it('author is satocw', () => {
-        assert(author === "aaa");   // will fail
+describe('オブジェクト同士の比較', () => {
+    let obj = { "a": "AAA", "b": "bbb" };
+    let newObj = obj;
+
+    it('newObj = obj と代入すると２つは同値', () => {
+        assert(obj === newObj);
+    });
+
+    it('片方のプロパティの値を変更してもまだ等しい', () => {
+        newObj.a = "CCC";
+        assert(obj === newObj);
+    });
+
+    it('片方のプロパティの値を変更するともう片方にも反映される', () => {
+        newObj.a = "DDD";
+        assert(obj.a === "DDD");
+    });
+
+    describe('階層が深い場合は', () => {
+        let nestedObj = {
+            "a": {
+                "b": "BBB"
+            }
+        };
+        let newNestedObj = nestedObj;
+
+        it('片方のプロパティの値を変更するともう片方にも反映される', () => {
+            newNestedObj.a.b = "CCC";
+            assert(nestedObj.a.b === "CCC");
+        });
+
+        it('片方のプロパティが違うオブジェクトを参照しても同じ', () => {
+            newNestedObj.a = { "b": "DDD" };
+            assert(nestedObj.a.b === "DDD");
+            assert(nestedObj === newNestedObj);
+            assert(nestedObj.a === newNestedObj.a);
+        });
     });
 });
